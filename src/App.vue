ee06c581280265268862a7a01499b93951b69bae
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-09-28 23:52:00 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="poblarSolicitudesDeCotizacion">
+      Poblar Solicitudes de Cotizacion
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import moment from "moment";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      metodos_de_pago: [
+        "Efectivo",
+        "Tarjeta de Credito",
+        "Tarjeta de Debito",
+        "Transferencia Bancaria",
+        "Cheque",
+      ],
+      condiciones_de_venta: [30, 60, 90],
+    };
+  },
+  methods: {
+    async poblarSolicitudesDeCotizacion() {
+      for (let i = 0; i < 10; i++) {
+        const randomDate =
+          new Date().getTime() + Math.floor(Math.random() * 10000000000);
+
+        const randomMetodo = Math.random() * (this.metodos_de_pago.length - 1);
+        const randomCondicion =
+          Math.random() * (this.condiciones_de_venta.length - 1);
+
+        const fecha_emision = moment(randomDate).format("YYYY-MM-DD");
+        const hora_emision = moment(randomDate).format("HH:mm:ss");
+
+        const metodo_de_pago = this.metodos_de_pago[Math.floor(randomMetodo)];
+        const condicion_de_venta =
+          this.condiciones_de_venta[Math.floor(randomCondicion)];
+
+        await axios.post(
+          `https://corev2.domhhv.cl/items/cotizaciones_proveedor`,
+          {
+            fecha_emision,
+            hora_emision,
+            "forma_de_pago": metodo_de_pago,
+            "condiciones_de_venta": condicion_de_venta,
+          }
+        );
+      }
+    },
+  },
+};
 </script>
 
 <style>
